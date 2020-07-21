f67bb4a885a5ab9f124a331b8fe8ed9cb7faef19
AURORA <fazalashrafofficial@gmail.com>
2020-07-21 14:55:49 +0530

    Hello world program

---
+++ Hello World.py
@@ -0,0 +1,9 @@
+""" This is a simple program that can display a message directly to the console,
+     to show how fun and elegant python is. 
+
+Authour: Fazal Ashraf ; Aurora
+Date:  Tuesday 21st July 2020,  14:52
+     
+"""
+
+print("Hello World!") # print is a predefined function in python to directly display message to the console.
